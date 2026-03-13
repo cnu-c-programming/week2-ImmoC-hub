18542c77de7b3475656ef05446442ac970818eda
seunghyeon <aig0320@naver.com>
2026-03-13 09:50:28 +0900

    1.2 complete

--- code/1-2.c
+++ code/1-2.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 int main()
 {
+    printf("% 5s ","name");
+    printf("%11s","id");
+    printf("% 10s","value\n");
 
+    printf("% 5s ","kim");
+    printf("%011d",12);
+    printf("% 10.2f\n",4.120000);
+
+    printf("% 5s ","lee");
+    printf("%011d",1922);
+    printf("% 10.2f\n",3.210000);
+
+    printf("% 5s ","park");
+    printf("%011d",432);
+    printf("% 10.2f\n",1.000000);
+
+    
 
     return 0;
 }
