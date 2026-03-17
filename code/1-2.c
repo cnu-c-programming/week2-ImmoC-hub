16d68ed12766aaaa999c7c4c44506ada05d9d349
seunghyeon <aig0320@naver.com>
2026-03-17 17:13:23 +0900

    end

--- code/1-2.c
+++ code/1-2.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    printf("% 5s ","name");
-    printf("%11s","id");
-    printf("% 10s","value\n");
+    printf("     % 5s ","name");
+    printf("%10s","id");
+    printf("      % 5s","value\n");
 
-    printf("% 5s ","kim");
-    printf("%011d",12);
-    printf("% 10.2f\n",4.120000);
+    printf("     % 5s ","kim");
+    printf("%010d",12);
+    printf("      % 5.2f\n",4.120000);
 
-    printf("% 5s ","lee");
-    printf("%011d",1922);
-    printf("% 10.2f\n",3.210000);
+    printf("     % 5s ","lee");
+    printf("%010d",1922);
+    printf("      % 5.2f\n",3.210000);
 
-    printf("% 5s ","park");
-    printf("%011d",432);
-    printf("% 10.2f\n",1.000000);
+    printf("     % 5s ","park");
+    printf("%010d",432);
+    printf("      % 5.2f\n",1.000000);
 
     
 
--- code/4-3.c
+++ code/4-3.c
@@ -11,7 +11,7 @@ int main()
             break;
         }
     }
-    if (flag){
+    if (flag&&num!=1){
         printf("true");
     }
     else{
